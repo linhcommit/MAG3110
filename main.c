650665391435494d1fe71267d608e2166370bfc9
Trần Hồng Quân <thquan2k3@gmail.com>
2024-05-26 21:55:38 +0700

    add more detail demo lcd

--- main.c
+++ main.c
@@ -1,15 +1,41 @@
-#include "MKL46Z4.h"
+#include "MKL46Z4.h"                    // Device:Startup
 #include "lcd.h"
 
 int main(void)
 {
 	LCD_Init();
 	uint16_t num = 0;
+	uint16_t num1 = 0;
 	while (1)
 	{
-		LCD_DisplayDemical(num++);
-		if (num >= 9999)
-			num = 0;
-		delay(1000000);
+		num = 0;
+		while(num++ < 120){
+			LCD_DisplayDemical(num);
+			delay(500000);
+		}
+		
+		num = 0;
+		num1 = 99;
+		while(num < 100){
+			LCD_DisplayTime(num,num1);
+			delay(500000);
+			num++;
+			num1--;
+		}
+		
+		LCD_DisplayError();
+		delay(30000000);
+		
+		LCD_DisplayAll();
+		delay(30000000);
+		
+		LCD_StartBlinkMode();
+		delay(30000000);
+		
+		LCD_StopBlinkMode();
+		delay(30000000);
+		
+		LCD_StopDisplay();
+		delay(30000000);
 	}
 }
